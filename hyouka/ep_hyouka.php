486202fbaef4b18dd665b0be238a3b82ccc65464
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-02 18:42:57 +0700

    Update ep_hyouka.php

--- hyouka/ep_hyouka.php
+++ hyouka/ep_hyouka.php
@@ -3,91 +3,91 @@
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่1",
 "ep":"https://proxy1.animeyoko.com/stream/720/H8OgKNFHl0DvB2e/__001",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep1.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep1.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่2",
 "ep":"https://firebasestorage.googleapis.com/v0/b/doome-308fc.appspot.com/o/test.mp4?alt=media&token=3f156283-a054-4959-a63f-23755e668220",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep2.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep2.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่3",
 "ep":"https://cdn.chavalit.is/vod/disk2/movies/ynlftt.mp4",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep3.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep3.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่4",
 "ep":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep4.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep4.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่5",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep5.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep5.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่6",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep6.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep6.jpg"
     },
     {
       "name_ep":"ปริศนาความทรงจำ ตอนที่7",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep7.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep7.jpg"
     },
   {
       "name_ep":"ปริศนาความทรงจำ ตอนที่8",
 "ep":"https://proxy1.animeyoko.com/stream/480/j5xxaaguFeqvNDw/__001",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep8.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep8.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่9",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep9.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep9.jpg"
     },
    {
       "name_ep":"ปริศนาความทรงจำ ตอนที่10",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep10.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep10.jpg"
     },
    {
       "name_ep":"ปริศนาความทรงจำ ตอนที่11",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep11.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep11.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่12",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep12.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep12.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่13",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep13.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep13.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่14",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep14.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep14.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่15",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep15.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep15.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่16",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep16.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep16.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่17",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep17.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep17.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่18",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep18.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep18.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่19",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep19.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep19.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่20",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep20.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep20.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่21",
- "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep21.jpg"
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep21.jpg"
     },
 {
       "name_ep":"ปริศนาความทรงจำ ตอนที่22 จบแล้ว",
