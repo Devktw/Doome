372c00ebe5b577785e61ac3ae1f99f2f26dbbab1
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-02 14:28:16 +0700

    Add files via upload

---
+++ ep_demonslayer.php
@@ -0,0 +1,9 @@
+{
+  "listanime":[
+{
+      "name_ep":"ดาบพิฆาตอสูรเดอะมูฟวี่",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":https://firebasestorage.googleapis.com/v0/b/doome-308fc.appspot.com/o/demonslayer1.png?alt=media&token=516fb91d-89fd-451e-8494-953b36b000e0"
+    }
+  ]
+}
