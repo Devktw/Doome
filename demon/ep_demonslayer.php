f963e2f59c71830d31b8dce456f4c5b523774291
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-02 15:23:59 +0700

    Update ep_demonslayer.php

--- demon/ep_demonslayer.php
+++ demon/ep_demonslayer.php
@@ -3,7 +3,7 @@
 {
       "name_ep":"ดาบพิฆาตอสูรเดอะมูฟวี่ ศึกรถไฟสู่นิรันดร์",
 "ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
- "subtitle":"พากย์ไทย", "image_ep":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1zhqd-dtNF0WQ77fb-yAOU3bWjfpBUOgd4g&usqp=CAU"
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demon/demonslayer1.png"
     }
   ]
 }
