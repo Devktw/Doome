9da8eea46fe40cc3dd0e1e1917505169dad17847
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-02 19:19:50 +0700

    Create ep_hyouka.php

---
+++ hyouka/ep_hyouka.php
@@ -0,0 +1,120 @@
+{
+  "listanime":[
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่1",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/4049978539/playlist.m3u8?anthony=6415ca2e506e57d1dbcce00fe004c40d&e=141931&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep1.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่2",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2443332942/playlist.m3u8?anthony=472e7d9474c901ce139b3ed52b6f2879&e=141975&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep2.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่3",
+"ep":"https://flyer.akamaicdn.cloud/auth/3869605336/playlist.m3u8?anthony=629c616b2ffeaa96fe8963808357478a&e=142078&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep3.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่4",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2141989986/playlist.m3u8?anthony=1074161bc56efddd0c3c920e1b9c02b9&e=142138&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep4.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่5",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep5.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่6",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2530183511/playlist.m3u8?anthony=faac8b1adc4477bc2e14a9a350728eda&e=142338&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep6.jpg"
+    },
+    {
+      "name_ep":"ปริศนาความทรงจำ ตอนที่7",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/4049978539/playlist.m3u8?anthony=31ba0160176b93667779516bef14c55d&e=142611&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep7.jpg"
+    },
+  {
+      "name_ep":"ปริศนาความทรงจำ ตอนที่8",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2443332942/playlist.m3u8?anthony=8c93d63f0cb0a2d7cdf14c4efd175f3c&e=142636&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep8.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่9",
+"ep":"https://flyer.akamaicdn.cloud/auth/264689901/playlist.m3u8?anthony=6bf42b0292d81aa71312f17c59134306&e=142842&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep9.jpg"
+    },
+   {
+      "name_ep":"ปริศนาความทรงจำ ตอนที่10",
+"ep":"https://flyer.akamaicdn.cloud/auth/2675557756/playlist.m3u8?anthony=b0852b8a6a1d9f819d44ad2260a2c30a&e=142877&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep10.jpg"
+    },
+   {
+      "name_ep":"ปริศนาความทรงจำ ตอนที่11",
+"ep":"https://flyer.akamaicdn.cloud/auth/3900634602/playlist.m3u8?anthony=f4df00d5a215401c36ee507595acb271&e=143144&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep11.jpg"
+    },
+{
+
+      "name_ep":"ปริศนาความทรงจำ ตอนที่11.5(OVA)",
+"ep":"https://flyer.akamaicdn.cloud/auth/2986418742/playlist.m3u8?anthony=81bcb69d27a6882425e93674c5ad6cf2&e=143098&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep11.jpg"
+
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่12",
+"ep":"https://flyer.akamaicdn.cloud/auth/372920705/playlist.m3u8?anthony=4b4f738ec17230c7b34c09067cabcf68&e=143244&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep12.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่13",
+"ep":"https://flyer.akamaicdn.cloud/auth/1631412503/playlist.m3u8?anthony=8d23fc4f7cd327e01f19fd52d4495dc2&e=143283&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep13.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่14",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/4164169901/playlist.m3u8?anthony=616b45fc3bbcc6f33eb28ddb57312bc9&e=143308&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep14.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่15",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2402484283/playlist.m3u8?anthony=d3bdeef63518695854480ae7becbce8c&e=143391&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep15.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่16",
+"ep":"https://flyer.akamaicdn.cloud/auth/290952600/playlist.m3u8?anthony=01d5dabafe1c9a4b2824d38ac1a10beb&e=143435&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep16.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่17",
+"ep":"https://flyer.akamaicdn.cloud/auth/1716561166/playlist.m3u8?anthony=a5a38b299dd7e64e8116507ba8e13cb3&e=143532&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep17.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่18",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/4284085428/playlist.m3u8?anthony=efbddb5f53513b5e5d902f7a81e5b238&e=143577&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep18.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่19",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2287911970/playlist.m3u8?anthony=d2ae53be0961e8a467d82386be1823a1&e=143650&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep19.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่20",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/417454515/playlist.m3u8?anthony=1213444dfc927795e8380ff9ea773a5a&e=143693&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep20.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่21",
+"ep":"https://flyer.akamaicdn.cloud/auth/1877403941/playlist.m3u8?anthony=001c982b9ffbe082c1fc99276e29d306&e=143708&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/hyouka/hotharo_ep21.jpg"
+    },
+{
+      "name_ep":"ปริศนาความทรงจำ ตอนที่22 จบแล้ว",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/253845696/playlist.m3u8?anthony=d662cc6d7695a3de860620c2970e2c2a&e=143765&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":"https://mrjovpn.tk/hotharo/hotharo_ep22.jpg"
+    }
+  ]
+}
