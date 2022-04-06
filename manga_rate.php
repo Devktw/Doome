29eb193a086b73178b812d65d5d121dd84937e76
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-06 10:01:58 +0700

    Add files via upload

---
+++ manga_rate.php
@@ -0,0 +1,34 @@
+ {
+  "listmanga":[
+{
+      "name_manga":"ขยี้จุดอ่อน",
+"url_ep":"https://mrjovpn.tk/manga_judont/judont_ep.php",
+      "year":"ปี: 2016",
+      "formation":"แนว: เรทอาร์(18+)",
+      "valus":"120 ตอน(ยังไม่จบ)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "sub_use":"ผู้แปล: นักแปลHiroki",
+      "view":"16 หน้า",
+"profile":"https://ni.123doujin.com/manhwa/orig_naniabfej.jpg",
+      "mini_story":"อัจฉริยะเท่านั้นที่จะได้ทำงานใหญ่ๆ และผมก็เป็นแค่คนที่ฉลาดกว่าที่ใช้ความสามารถนั้นให้เป็นประโยชน์จะเป็นอย่างไรไปดูกันเลย",
+      "image_manga":"https://ni.123doujin.com/manhwa/orig_naniabfej.jpg"
+
+    },
+{
+      "name_manga":"พี่สาวข้างบ้าน",
+"url_ep":"https://mrjovpn.tk/manga_sister/sister_ep.php",
+      "year":"ปี: 2016",
+      "formation":"แนว: เรทอาร์(18+)",
+      "valus":"108 ตอน(กำลังอัปเดต)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+  "sub_use":"ผู้แปล: นักแปลHiroki",
+      "view":"16 หน้า",
+"profile":"https://doujinza.com/wp-content/uploads/2021/01/msedge_P7breYI400-193x278.png",
+      "mini_story":"เรื่องราวของจีโฮที่อาศัยอยู่กับพี่สาวของเขาเรื่องราวจะเป็นยังไงติดตามชม!!",
+      "image_manga":"https://doujinza.com/wp-content/uploads/2021/01/msedge_P7breYI400-193x278.png"
+
+    }
+  ]
+}
---
+++ mangadoome.php
@@ -0,0 +1,78 @@
+ {
+  "listmanga":[
+
+{
+      "name_manga":"ตำนาน ราชันเซียนแห่งนคร",
+"url_ep":"https://mrjovpn.tk/manga_thecity/thecity_ep.php",
+      "year":"ปี: 2020",
+      "formation":"แนว: ต่างโลก, เหนือธรรมชาติ, แฟนตาซี, แอคชั่น",
+      "valus":"130 ตอน(กำลังอัปเดต)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+"sub_use":"ผู้แปล: ไม่ทราบ",
+      "view":"16 หน้า",
+"profile":"https://manga-titan.com/wp-content/uploads/WP-manga/data/manga_5ef47a79d6137/56f4dbc5e6cc95c2247e4e6b960c6349/1.jpg",
+      "mini_story":"ในยุคนี้ผู้คนมักจะกลับชาติมาเกิด แต่ ( ลินเฟย ) ขึ้นอยู่กับการเป็นอมตะ ฉันอยู่มาหลายร้อยล้านปีเพื่อตามหาคนที่รักที่ฉันหายไปเป็นล้านปี !!",
+      "image_manga":"https://manga-titan.com/wp-content/uploads/WP-manga/data/manga_5ef47a79d6137/56f4dbc5e6cc95c2247e4e6b960c6349/1.jpg"
+
+    },
+{
+      "name_manga":"พี่สาวข้างบ้าน",
+"url_ep":"https://mrjovpn.tk/manga_sister/sister_ep.php",
+      "year":"ปี: 2016",
+      "formation":"แนว: เรทอาร์(18+)",
+      "valus":"108 ตอน(กำลังอัปเดต)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+  "sub_use":"ผู้แปล: นักแปลHiroki",
+      "view":"16 หน้า",
+"profile":"https://doujinza.com/wp-content/uploads/2021/01/msedge_P7breYI400-193x278.png",
+      "mini_story":"เรื่องราวของจีโฮที่อาศัยอยู่กับพี่สาวของเขาเรื่องราวจะเป็นยังไงติดตามชม!!",
+      "image_manga":"https://doujinza.com/wp-content/uploads/2021/01/msedge_P7breYI400-193x278.png"
+
+    },
+{
+      "name_manga":"ขยี้จุดอ่อน",
+"url_ep":"https://mrjovpn.tk/manga_judont/judont_ep.php",
+      "year":"ปี: 2016",
+      "formation":"แนว: เรทอาร์(18+)",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+"sub_use":"ผู้แปล: นักแปลHiroki",
+      "view":"16 หน้า",
+"profile":"https://ni.123doujin.com/manhwa/orig_naniabfej.jpg",
+      "mini_story":"อัจฉริยะเท่านั้นที่จะได้ทำงานใหญ่ๆ และผมก็เป็นแค่คนที่ฉลาดกว่าที่ใช้ความสามารถนั้นให้เป็นประโยชน์จะเป็นอย่างไรไปดูกันเลย",
+      "image_manga":"https://ni.123doujin.com/manhwa/orig_naniabfej.jpg"
+
+    },
+{
+      "name_manga":"Eternal first son-in-law",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+"sub_use":"ผู้แปล: นักแปลHiroki",
+      "mini_story":"ทันจิโร่ เด็กหนุ่มขายถ่ายผู้มีจิตใจอ่อนโยนได้ออกเดินทางเพื่อที่จะ
+โค่นล้มอสูรที่สังหารครอบครัวและช่วยให้เนซิโกะ น้องสาวที่โดน
+เปลี่ยนเป็นอสูรกลับเป็นมนุษย์ดังเดิม",
+      "image_manga":"https://mangafin.com/wp-content/uploads/2021/09/420_vCwK6l1-193x278.jpg"
+
+    },
+{
+      "name_manga":"ดาบพิฆาตอสูร",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+"sub_use":"ผู้แปล: นักแปลHiroki",
+      "mini_story":"ทันจิโร่ เด็กหนุ่มขายถ่ายผู้มีจิตใจอ่อนโยนได้ออกเดินทางเพื่อที่จะ
+โค่นล้มอสูรที่สังหารครอบครัวและช่วยให้เนซิโกะ น้องสาวที่โดน
+เปลี่ยนเป็นอสูรกลับเป็นมนุษย์ดังเดิม",
+      "image_manga":"https://manga00.com/wp-content/uploads/2020/11/179023.jpg.webp"
+
+    }
+  ]
+}
