9ba6e3448646eb717f993ef689115b58c59f10d9
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-07 12:39:45 +0700

    Update ep_demonslayer.php

--- demonss2/ep_demonslayer.php
+++ demonss2/ep_demonslayer.php
@@ -1,1 +1,59 @@
-o
+{
+  "listanime":[
+{
+      "name_ep":"ตอนที่1 เสาหลักเสียง อุซุย เท็นเง็น",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3006645047/playlist.m3u8?anthony=58eb5785fee21639a7e37fea7fde95ce&e=151459&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep1.jpg"
+    },
+{
+      "name_ep":"ตอนที่2 ลักรอบเข้าย่ายโคมแดง",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3291673505/playlist.m3u8?anthony=f901347b671a9636d7c652192f8b1fe2&e=151477&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep2.jpg"
+    },
+{
+      "name_ep":"ตอนที่3 ผู้ใดกัน",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/1564189211/playlist.m3u8?anthony=c620bcaaa40dfa36ba5fbbd436208642&e=151517&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep3.jpg"
+    },
+{
+      "name_ep":"ตอนที่4 ค่ำคืนนี้",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/708612749/playlist.m3u8?anthony=4649c10e0eabc513fd2b7bc0e63d22a9&e=151542&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep4.jpg"
+    },
+{
+      "name_ep":"ตอนที่5 ออกไปลุยอย่างอลังการ",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3025667886/playlist.m3u8?anthony=1bad15f857bb78c9513a68ed9911a73a&e=151584&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep5.jpg"
+    },
+{
+      "name_ep":"ตอนที่6 ความทรงจำที่ทับซ้อน",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3277797304/playlist.m3u8?anthony=639376c19f562f2c2f746f335b57921e&e=151599&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep6.jpg"
+    },
+    {
+      "name_ep":"ตอนที่7 เปลี่ยนรูปลักษณ์",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/1515611650/playlist.m3u8?anthony=e7ad0b9cd05db637d805db52db0c41cc&e=151627&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep7.jpg"
+    },
+ {
+      "name_ep":"ตอนที่8 รวมพล",
+"ep":"https://flyer.akamaicdn.cloud/auth/760305300/playlist.m3u8?anthony=b46914f3543c28fd57261d700de63a8a&e=151658&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep8.jpg"
+    },
+{
+      "name_ep":"ตอนที่9 หากโค่นอสูรข้างขึ้นได้ล่ะก็",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3186510597/playlist.m3u8?anthony=34bd60657c9b830538b14805c28b5219&e=151687&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep9.jpg"
+    },
+ {
+      "name_ep":"ตอนที่10 ห้ามยอมแพ้เด็ดขาด",
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3404299155/playlist.m3u8?anthony=73006adf067d34aaca6e524f5caf661b&e=151733&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep10.jpg"
+    },
+   {
+      "name_ep":"ตอนที่11 ไม่ว่าจะเกิดใหม่อีกกี่ครั้ง",
+"ep":"https://flyer.akamaicdn.cloud/auth/883810296/playlist.m3u8?anthony=eff61e8c9f1ce12b168100d20f64321b&e=151777&a=51.79.147.28",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demonss2/demonss2_ep11.jpg"
+    }
+  ]
+}
