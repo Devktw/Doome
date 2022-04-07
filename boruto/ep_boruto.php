5117cdb29c1824cf3ac15cab9c3e2d27bcc8c83e
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-07 18:00:11 +0700

    Update ep_boruto.php

--- boruto/ep_boruto.php
+++ boruto/ep_boruto.php
@@ -4,6 +4,6 @@
       "name_ep":"ตอนที่1 อุสึมากิ โบรูโตะ",
 "ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/937535520/playlist.m3u8?anthony=30683611dde48e35e35fcac94cafc686&e=169556&a=51.79.147.28",
  "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/boruto/boruto_ep1.jpg"
-    }
+    },
 ]
 }
