dbfbead163d8454ec02a272b6b2d05a6dff6630b
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-09 16:08:46 +0700

    Update sister_ep.php

--- manga_sister/sister_ep.php
+++ manga_sister/sister_ep.php
@@ -10,7 +10,7 @@
       "seson":"ซีซั่น: 1",
       "subtitle":"แปลไทย",
       "view":"54 หน้า",
-"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep1.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep1.jpg"
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep1.jpg"
 
     },
 {
