b2f9145b0061ae640fb4e14620ef459ec17a5e4a
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-11 08:48:49 +0700

    Update ep_youname.php

--- youname/ep_youname.php
+++ youname/ep_youname.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"หลับตาฝันถึงชื่อเธอ",
-"ep":"https://cdn.chavalit.is/vod/disk2/movies/ynlftt.mp4",
+"ep":"https://a12.rukoluo.com/d/Your-Name/Your-Name-TH.mp4?m=J5uNfJtSWJ_6sa-n3Fsl9Q&e=1649652466",
  "subtitle":"ซับไทย", "image_ep":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1zhqd-dtNF0WQ77fb-yAOU3bWjfpBUOgd4g&usqp=CAU"
     }
   ]
