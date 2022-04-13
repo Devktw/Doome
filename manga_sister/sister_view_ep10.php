0d855ff066bdb666983c75d204d41cb3812e47b3
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-13 12:00:22 +0700

    Add files via upload

---
+++ manga_sister/sister_view_ep10.php
@@ -0,0 +1,56 @@
+ {
+  "listmanga":[
+{
+"view_manga_a4":"หน้าที่1",
+"view_manga":"https://ilovedoujin.com/i/1073/001.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่2",
+"view_manga":"https://ilovedoujin.com/i/1073/002.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่3",
+"view_manga":"https://ilovedoujin.com/i/1073/003.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่4",
+"view_manga":"https://ilovedoujin.com/i/1073/004.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่5",
+"view_manga":"https://ilovedoujin.com/i/1073/005.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่6",
+"view_manga":"https://ilovedoujin.com/i/1073/006.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่7",
+"view_manga":"https://ilovedoujin.com/i/1073/007.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่8",
+"view_manga":"https://ilovedoujin.com/i/1073/008.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่9",
+"view_manga":"https://ilovedoujin.com/i/1073/009.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่10",
+"view_manga":"https://ilovedoujin.com/i/1073/010.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่11",
+"view_manga":"https://ilovedoujin.com/i/1073/011.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่12",
+"view_manga":"https://ilovedoujin.com/i/1073/012.jpg"
+    },
+{
+"view_manga_a4":"หน้าที่13",
+"view_manga":"https://ilovedoujin.com/i/1073/013.jpg"
+    }
+  ]
+}
