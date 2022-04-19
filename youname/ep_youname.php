0d026d8bd722b269c6bdb204cdf03a1a8205dcb5
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-19 17:20:29 +0700

    Update ep_youname.php

--- youname/ep_youname.php
+++ youname/ep_youname.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"หลับตาฝันถึงชื่อเธอ",
-"ep":"https://proxy1.animeyoko.com/stream/720/eWxp9Q06QCRjGlW/__001",
+"ep":"https://server1.animelizm.xyz/hls/tU7l2L0/file_,720,.mp4.urlset/master.m3u8",
  "subtitle":"ซับไทย", "image_ep":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1zhqd-dtNF0WQ77fb-yAOU3bWjfpBUOgd4g&usqp=CAU"
     }
   ]
