ef343a6bd7211659a67309f38c1e6d37b36dcedc
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-19 17:11:41 +0700

    Update ep_youname.php

--- youname/ep_youname.php
+++ youname/ep_youname.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"หลับตาฝันถึงชื่อเธอ",
-"ep":"https://a12.rukoluo.com/d/Your-Name/Your-Name-TH.mp4?m=J5uNfJtSWJ_6sa-n3Fsl9Q&e=1649652466",
+"ep":"https://proxy1.animeyoko.com/stream/720/eWxp9Q06QCRjGlW/__001",
  "subtitle":"ซับไทย", "image_ep":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1zhqd-dtNF0WQ77fb-yAOU3bWjfpBUOgd4g&usqp=CAU"
     }
   ]
