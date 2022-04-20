1d81b6f3db5a53496d2f32f5665410813c0708bb
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-20 15:55:43 +0700

    Create sister_ep.php

---
+++ manga_sister/sister_ep.php
@@ -0,0 +1,396 @@
+ {
+  "listmanga":[
+{
+
+      "name_manga_ep":"ตอนที่1",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"54 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep1.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep1.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่2",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"38 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep2.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep2.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่3",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"39 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep3.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep3.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่4",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"34 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep4.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep4.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่5",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"40 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep5.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep5.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่6",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"35 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep6.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep6.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่7",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"37 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep7.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep7.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่8",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"35 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep8.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep8.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่9",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep9.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep9.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่10",
+      "ep_url":"",
+      "year":"ปี: 2016",
+      "formation":"แนว: แอ็คชั่น",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"13 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep10.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep10.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่11",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep11.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep11.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่12",
+
+      "subtitle":"แปลไทย",
+      "view":"12 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep12.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep12.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่13",
+
+      "subtitle":"แปลไทย",
+      "view":"14 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep13.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep13.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่14",
+
+      "subtitle":"แปลไทย",
+      "view":"12 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep14.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep14.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่15",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep15.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep15.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่16",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep16.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep16.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่17",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep17.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep17.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่18",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep18.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep18.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่19",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep19.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep19.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่20",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep20.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep20.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่21",
+
+      "subtitle":"แปลไทย",
+      "view":"8 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep21.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep21.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่22",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep22.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep22.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่23",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep23.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep23.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่24",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep24.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep24.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่25",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep25.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep25.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่26",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep26.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep26.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่27",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep27.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep27.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่28",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep28.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep28.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่29",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep29.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep29.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่30",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep30.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep30.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่31",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep31.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep31.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่32",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep32.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep32.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่33",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep33.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep33.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่34",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep34.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep34.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่35",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep35.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep35.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่36",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep36.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep36.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่37",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep37.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep37.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่38",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep38.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep38.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่39",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep39.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep39.jpg"
+
+    },
+{
+
+     
