63647c7a625eac3c4bd7a594303ce8e8aabeae18
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-20 16:06:36 +0700

    Create sister_ep.php

---
+++ manga_sister/sister_ep.php
@@ -0,0 +1,320 @@
+tps://mrjovpn.tk/manga_sister/sister_view_ep73.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep73.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่74",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep74.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep74.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่75",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep75.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep75.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่76",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep76.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep76.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่77",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep77.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep77.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่78",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep70.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep70.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่79",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep79.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep79.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่80",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep80.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep80.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่81",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep81.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep81.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่82",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep82.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep82.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่83",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep83.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep83.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่84",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep84.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep84.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่85",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep85.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep85.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่86",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep86.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep86.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่87",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep87.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep87.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่88",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep88.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep88.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่89",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep89.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep89.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่90",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep90.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep90.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่91",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep91.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep91.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่92",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep92.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep92.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่93",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep93.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep93.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่94",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep94.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep94.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่95",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep95.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep95.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่96",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep96.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep96.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่97",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep97.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep97.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่98",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep98.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep98.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่99",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep99.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep99.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่100",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep100.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep100.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่101",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep101.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep101.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่102",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep102.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep102.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่103",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep103.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep103.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่104",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep104.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep104.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่105",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep105.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep105.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่106",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep106.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep106.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่107",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep107.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep107.jpg"
+
+    },
+{
+
+      "name_manga_ep":"ตอนที่108",
+
+      "subtitle":"แปลไทย",
+      "view":"11 หน้า",
+"image_view_ep":"https://mrjovpn.tk/manga_sister/sister_view_ep108.php", "image_manga_ep":"https://mrjovpn.tk/manga_sister/sister_ep108.jpg"
+
+    }
+  ]
+}
