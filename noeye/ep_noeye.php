cadff827b89777ac7b2be36ae5700d022369b052
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-22 12:06:58 +0700

    Create ep_noeye.php

---
+++ noeye/ep_noeye.php
@@ -0,0 +1,24 @@
+{
+  "listanime":[
+{
+      "name_ep":"ตอนที่1",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
+    }
+{
+      "name_ep":"ตอนที่2",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep2.jpg"
+    },
+{
+      "name_ep":"ตอนที่3",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep3.jpg"
+    },
+{
+      "name_ep":"ตอนที่4",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+ "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep4.jpg"
+    }
+  ]
+}
