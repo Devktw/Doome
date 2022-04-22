f0c6924f104e26235411ce0a8bb58aea0ecc8e11
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-22 12:20:47 +0700

    Update ep_noeye.php

--- noeye/ep_noeye.php
+++ noeye/ep_noeye.php
@@ -2,22 +2,22 @@
   "listanime":[
 {
       "name_ep":"ตอนที่1",
-"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+"ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1",
  "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
     }
 {
       "name_ep":"ตอนที่2",
-"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+"ep":"https://hls-nani.xyz/cdn/hls/fa2aea23ca29ac615c62dd33efe42630/1/index-v1-a1.m3u8?s=1",
  "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep2.jpg"
     },
 {
       "name_ep":"ตอนที่3",
-"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+"ep":"https://hls-nani.xyz/cdn/hls/f223377f57432c92d62a65e5a005871b/1/index-v1-a1.m3u8?s=1",
  "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep3.jpg"
     },
 {
       "name_ep":"ตอนที่4",
-"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186",
+"ep":"https://hls-nani.xyz/cdn/hls/b97f5918af84949b3e28a8fb2cb35d24/1/index-v1-a1.m3u8?s=1",
  "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep4.jpg"
     }
   ]
