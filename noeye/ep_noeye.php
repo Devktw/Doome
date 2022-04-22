021f34f04ecb85d3868dff9e4cb3e0edae4ab0cb
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-22 12:27:50 +0700

    Update ep_noeye.php

--- noeye/ep_noeye.php
+++ noeye/ep_noeye.php
@@ -4,7 +4,7 @@
       "name_ep":"ตอนที่1",
 "ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1",
  "subtitle":"ซับไทย", "image_ep":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
-    }
+    },
 {
       "name_ep":"ตอนที่2",
 "ep":"https://hls-nani.xyz/cdn/hls/fa2aea23ca29ac615c62dd33efe42630/1/index-v1-a1.m3u8?s=1",
