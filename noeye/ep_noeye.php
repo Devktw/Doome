24c0142c396ac8c02e0b34dc74f853277f39f274
Devktw <92218628+Devktw@users.noreply.github.com>
2022-04-22 12:44:00 +0700

    Create ep_noeye.php

---
+++ noeye/ep_noeye.php
@@ -0,0 +1,9 @@
+{
+  "listanime":[
+{
+      "name_ep":"หลับตาฝันถึงชื่อเธอ",
+"ep":"https://files.akuma-player.xyz/view/1cb2cb19-4bd2-529c-b9c2-49dc5f351075.m3u8",
+ "subtitle":"ซับไทย", "image_ep":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1zhqd-dtNF0WQ77fb-yAOU3bWjfpBUOgd4g&usqp=CAU"
+    }
+  ]
+}
