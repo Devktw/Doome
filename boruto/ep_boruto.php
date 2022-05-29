20373eb487b62e3e38a9e5c4179cb7cbaac14716
Devktw <92218628+Devktw@users.noreply.github.com>
2022-05-29 08:24:21 +0700

    Update ep_boruto.php

--- boruto/ep_boruto.php
+++ boruto/ep_boruto.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"ตอนที่1 อุสึมากิ โบรูโตะ",
-"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/937535520/playlist.m3u8?anthony=30683611dde48e35e35fcac94cafc686&e=169556&a=51.79.147.28",
+"ep":"https://fvs.io/redirector?token=THh3MC9kNU1TWWNWeDVucmlWSUcySVJoMlRTdE5OR29HUlVycGZoeWhaOWNJS1g2d245UUVtRHhweCt3a1B2b3l2cWVZU25YTFhPbURJQVVYY0tNVDNiWWlpa2tNUHdqNk5KS2VyMUhvYVlvb0tXdktsMHVxWENuZWM3OHM5QjVQT0xyaWRKbWxlT2lBbWRYVWY0WU1qRXZtZDBzQStEUE1RPT06ajNPdjVVQ2Z4YWVuYmhnODBBaHhNZz09CBBS",
  "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/boruto/boruto_ep1.jpg"
     }
 ]
