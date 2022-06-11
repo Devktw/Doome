c02cbe6c020b4fb1c63c322e97d0f677363c22a5
Devktw <92218628+Devktw@users.noreply.github.com>
2022-06-12 06:11:16 +0700

    Update ep_boruto.php

--- boruto/ep_boruto.php
+++ boruto/ep_boruto.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"ตอนที่1 อุสึมากิ โบรูโตะ",
-"ep":"https://fvs.io/redirector?token=S25veW9GeEF0RlM3ZHgxSzdlaklvV0d5NDN4amlhbGljWnpQZC85ZU13SUxUTUhXaE9FQVZ5NGlpV3hsNW5nNUM1V001YmRiZDF1NVZhMHNOL0J0Um5sOFA2TkNWR2lhdGduZmhWbmlqTVhxOTJ2dEVVMDBGMm1kUUZ4a3p3cWNNMGkrV1ZoUDJoc0tnRXdPNGVZY1NscnhKaFgvZlQ3eEJBPT06RjQyLzdVVStReThONzl1czUrTFA4QT09FWTy",
+"ep":"https://cdn.badroids.is/vod/disk2/movies/irmnmk.mp4",
  "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/boruto/boruto_ep1.jpg"
     }
 ]
