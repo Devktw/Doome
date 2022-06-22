b521662093d8b12438d5a07058babfd173613fa2
Devktw <92218628+Devktw@users.noreply.github.com>
2022-06-22 17:28:13 +0700

    Update 200iq.php

--- 200iq.php
+++ 200iq.php
@@ -1,12 +1,10 @@
 {
   "listitem":[
 {
-      "name_item":"ชื่อนักวิทยาศาสตร์",
-"ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1", "image_item":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
+      "name_item":"ชื่อนักวิทยาศาสตร์", "image_item":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
     },
 {
-      "name_item":"สูตรการหาพื้นที่3เหลี่ยม",
-"ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1", "image_item":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
+      "name_item":"สูตรการหาพื้นที่3เหลี่ยม", "image_item":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
     }
   ]
 }
