24af5d38ca22358543dceea0fbc05692e1203111
Devktw <92218628+Devktw@users.noreply.github.com>
2022-06-23 07:12:22 +0700

    Update 200iq.php

--- 200iq.php
+++ 200iq.php
@@ -1,10 +1,14 @@
 {
   "listitem":[
 {
-      "name_item":"ชื่อนักวิทยาศาสตร์", "image_item":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
+      "name_item":"ตอนที่1",
+"ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1",
+ "subtitle":"ซับไทย", "image_item":"https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
     },
 {
-      "name_item":"สูตรการหาพื้นที่3เหลี่ยม", "image_item":https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
+      "name_item":"ตอนที่2",
+"ep":"https://hls-nani.xyz/cdn/hls/fa2aea23ca29ac615c62dd33efe42630/1/index-v1-a1.m3u8?s=1",
+ "subtitle":"ซับไทย", "image_item":"https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep2.jpg"
     }
   ]
 }
