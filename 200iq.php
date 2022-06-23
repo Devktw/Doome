8420875cdf782ef8efaf04b75aa73f7578521e38
Devktw <92218628+Devktw@users.noreply.github.com>
2022-06-23 07:29:01 +0700

    Update 200iq.php

--- 200iq.php
+++ 200iq.php
@@ -1,7 +1,7 @@
 {
   "listitem":[
 {
-      "name_item":"ตอนที่1",
+      "name_item":"รายชื่อนักวิทยาศาสตร์",
 "ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1",
  "subtitle":"ซับไทย", "image_item":"https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
     },
