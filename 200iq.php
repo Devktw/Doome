cb2199fadf361670b2fefaf67cae5ca7944a9c10
Devktw <92218628+Devktw@users.noreply.github.com>
2022-07-06 18:29:18 +0700

    Update 200iq.php

--- 200iq.php
+++ 200iq.php
@@ -1,14 +1,19 @@
 {
-  "listitem":[
+  "data":[
 {
-      "name_item":"รายชื่อนักวิทยาศาสตร์",
-"ep":"https://hls-nani.xyz/cdn/hls/cf0efaf25d54cc2f394d58073c0671ed/1/index-v1-a1.m3u8?s=1",
- "subtitle":"ซับไทย", "image_item":"https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep1.jpg"
+      "name":"วันจันทร์"
     },
 {
-      "name_item":"ตอนที่2",
-"ep":"https://hls-nani.xyz/cdn/hls/fa2aea23ca29ac615c62dd33efe42630/1/index-v1-a1.m3u8?s=1",
- "subtitle":"ซับไทย", "image_item":"https://github.com/Devktw/Doome/raw/main/noeye/noeye_ep2.jpg"
+      "name":"วันอังคาร"
+    },
+{
+      "name":"วันพุธ"
+    },
+{
+      "name":"วันพฤหัสบดี"
+    },
+{
+      "name":"วันศุกร์"
     }
   ]
 }
