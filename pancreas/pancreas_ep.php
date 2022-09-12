62be635556708935c89c0441f10511a08b49a02a
Devktw <92218628+Devktw@users.noreply.github.com>
2022-09-12 20:18:13 +0700

    Update pancreas_ep.php

--- pancreas/pancreas_ep.php
+++ pancreas/pancreas_ep.php
@@ -3,7 +3,7 @@
 {
       "name_ep":"เพราะหัวใจใกล้ตับอ่อนเดอะมูฟวี่",
 "ep":"https://files.akuma-player.xyz/view/f0752c91-6de5-5a7c-be6d-5f9ca7799d51.m3u8",
- "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/panteas/IMG_20220912_200527.jpg"
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/pancreas/IMG_20220912_200527.jpg"
     }
   ]
 }
