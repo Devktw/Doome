ec87c98189c207c7df64bb743fa42829386281be
Devktw <92218628+Devktw@users.noreply.github.com>
2022-09-13 17:29:21 +0700

    Update ep_anos.php

--- anos/ep_anos.php
+++ anos/ep_anos.php
@@ -1,1 +1,137 @@
-0
+{
+
+  "listanime":[
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่1",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/3131475559/playlist.m3u8?anthony=4b1e22c42a6f2476c1e685a6d472d178&e=105927&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep1.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่2",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/1085200064/playlist.m3u8?anthony=a88e76bb127577ccd3deaa891d64541b&e=105960&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep2.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่3",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/204065023/playlist.m3u8?anthony=5711ab3dd431f88a2fa43a814f591b5a&e=106000&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep3.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่4",
+
+"ep":"https://flyer.akamaicdn.cloud/auth/476253624/playlist.m3u8?anthony=90635db7c55864c52517f1776c9c6736&e=106045&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep4.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่5",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/155250217/playlist.m3u8?anthony=cee809ef03f71558953edc8216c4bb0b&e=106075&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep5.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่6",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/233546440/playlist.m3u8?anthony=09f43bb628781d5b85260dd96b29c6c0&e=106150&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep6.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่7",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2322841095/playlist.m3u8?anthony=c5f64498f9c1fbd1f420fe044c7b0270&e=106188&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep7.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่8",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/820122140/playlist.m3u8?anthony=27a8c096309d229487f407079ac64223&e=106234&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep8.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่9",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/1418647320/playlist.m3u8?anthony=cc2e032a93d519587895863da72fa340&e=106269&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep9.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่10",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/1280805998/playlist.m3u8?anthony=0b20326e51ab25b2831be5252381a44d&e=106302&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep10.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่11",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/2022448212/playlist.m3u8?anthony=0ff0a64fb71ef6bc4d4ab516ce16f8c4&e=106341&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep11.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่12",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/627316022/playlist.m3u8?anthony=2aaea70e470997e6878a94cf4c188cc9&e=106373&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep12.jpg"
+
+    },
+
+{
+
+      "name_ep":"ใครว่าข้าไม่เหมาะเป็นจอมมาร ตอนที่13",
+
+"ep":"https://ojkiydmujl6f.akamaicdn.cloud/auth/1181029419/playlist.m3u8?anthony=7815db631dc32f851bc220c5644ac688&e=106406&a=116.204.180.52",
+
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/anos/anos_ep13.jpg"
+
+    }
+
+]
+
+}
