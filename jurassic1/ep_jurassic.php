92b644200ee862fd28cfe9d6de9be75f8c76eb41
Devktw <92218628+Devktw@users.noreply.github.com>
2022-09-24 11:06:47 +0700

    Update ep_jurassic.php

--- jurassic1/ep_jurassic.php
+++ jurassic1/ep_jurassic.php
@@ -1,1 +1,9 @@
-0
+{
+  "listanime":[
+{
+      "name_ep":"ดาบพิฆาตอสูร เดอะมูฟวี่ ศึกรถไฟสู่นิรันดร์",
+"ep":"https://cloudway.cloud/api/m3u8/LnYzJ1Rpz2vsubDxIt1Hu7OunC33u1cxcCBU9Q4k/default",
+ "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/jurassic1/jurassic.png"
+    }
+  ]
+}
