35179cc9159ab5debfae0268696ce7dbe4b347fe
Devktw <92218628+Devktw@users.noreply.github.com>
2022-10-13 11:45:18 +0700

    Update ep_chainsawman.php

--- chainsawman/ep_chainsawman.php
+++ chainsawman/ep_chainsawman.php
@@ -1,1 +1,9 @@
-9
+{
+  "listanime":[
+{
+      "name_ep":"chainsawman ตอนที่1",
+"ep":"https://flyer.akamaicdn.cloud/auth/2227206714/playlist.m3u8?anthony=c2f905392b7978e38a9b8f057132a37e&e=178093&a=103.2.115.135",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/chainsawman/chainsawman_ep1.jpg"
+    }
+  ]
+}
