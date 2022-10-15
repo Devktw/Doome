dffd3409f3c3f65c0812561ebff82e268d08cae8
Devktw <92218628+Devktw@users.noreply.github.com>
2022-10-15 08:01:41 +0700

    Update ep_jurassic.php

--- jurassic1/ep_jurassic.php
+++ jurassic1/ep_jurassic.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"จูราสสิคเวิลด์",
-"ep":"https://proxy2-cdn1.cdn9x.com/r92dub8f/index.m3u8vMjAyMiAxMjo0MTo1NSBBTSZoYXNoX3ZhbHVlPXVSWFlsMEZoTjlnN3NaWlJmMDBYTnc9PSZ2YWxpZG1pbnV0ZXM9NjA=",
+"ep":"https://proxy2-cdn1.cdn9x.com/r92dub8f/index.m3u8",
  "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/jurassic1/jurassic.png"
     }
   ]
