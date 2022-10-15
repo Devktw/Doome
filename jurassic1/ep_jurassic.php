9131aafd3b4354ac1cdd45a0749bd908e9714005
Devktw <92218628+Devktw@users.noreply.github.com>
2022-10-15 07:45:52 +0700

    Update ep_jurassic.php

--- jurassic1/ep_jurassic.php
+++ jurassic1/ep_jurassic.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"จูราสสิคเวิลด์",
-"ep":"https://cloudway.cloud/api/m3u8/LnYzJ1Rpz2vsubDxIt1Hu7OunC33u1cxcCBU9Q4k/default",
+"ep":"https://cdn-21.vip-streaming.com:30443/imovieth/2-ssd/movie/store-15/The_Martian_2015_th_1080p.mp4/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTAvMTUvMjAyMiAxMjo0MTo1NSBBTSZoYXNoX3ZhbHVlPXVSWFlsMEZoTjlnN3NaWlJmMDBYTnc9PSZ2YWxpZG1pbnV0ZXM9NjA=",
  "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/jurassic1/jurassic.png"
     }
   ]
