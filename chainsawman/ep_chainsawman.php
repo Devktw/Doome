5d7565489d649053a07b061cd1c038833764730a
Devktw <92218628+Devktw@users.noreply.github.com>
2022-10-20 07:41:56 +0700

    Update ep_chainsawman.php

--- chainsawman/ep_chainsawman.php
+++ chainsawman/ep_chainsawman.php
@@ -4,6 +4,11 @@
       "name_ep":"chainsawman ตอนที่1",
 "ep":"https://flyer.akamaicdn.cloud/auth/2227206714/playlist.m3u8?anthony=c2f905392b7978e38a9b8f057132a37e&e=178093&a=103.2.115.135",
  "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/chainsawman/chainsawman_ep1.jpg"
+    },
+{
+      "name_ep":"chainsawman ตอนที่2",
+"ep":"https://flyer.akamaicdn.cloud/auth/2040917636/playlist.m3u8?anthony=9a2178c2e39b9a50c2295fd9d3fa4c84&e=167263&a=116.204.180.52",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/chainsawman/chainsawman_ep2.jpg"
     }
   ]
 }
