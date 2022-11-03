8aa20c46bb8e1840f3ce6b5c6c0a9189aa3e9e1f
Devktw <92218628+Devktw@users.noreply.github.com>
2022-11-03 10:27:47 +0700

    Update cc_demon.php

--- demon/cc_demon.php
+++ demon/cc_demon.php
@@ -1,1 +1,13 @@
-P
+{
+  "listanime":[
+{
+      "cc":"ซับไทย",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186"
+   },
+{
+      "cc":"พากย์ไทย",
+"ep":"https://flyer.akamaicdn.cloud/auth/1870300661/playlist.m3u8?anthony=dc4e4a535cee9fe6ff301f6c445a34ea&e=149333&a=116.204.182.186"
+   }
+  ]
+}
+
