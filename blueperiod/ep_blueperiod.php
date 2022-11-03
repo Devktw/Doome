8577b2a9e91e1bfd7384dfba724c05bf04fc7b47
Devktw <92218628+Devktw@users.noreply.github.com>
2022-11-03 07:06:30 +0700

    Update and rename ep_blueperiod to ep_blueperiod.php

---
+++ blueperiod/ep_blueperiod.php
@@ -0,0 +1,64 @@
+{
+  "listanime":[
+{
+      "name_ep":"blueperiod ตอนที่1",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953583765077237760/432f49a4fe98e624338b1e99f2d66ffa.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep1.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่1",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953579543870963742/6eb666b578d64dc268b640accdfc83d0.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep2.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่3",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953595146962882640/484f31bce32ac14314aa0b7b2c869b65.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep3.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่4",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953580625095454730/7379f9d76dd40d516cadf5186d03fc86.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep4.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่5",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953581863379795998/03473bf365dd63441087ec0d38258ffe.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep5.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่6",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953580998371725333/6f6451cbc1ac1fe1ffb4c928cf9764d9.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep6.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่7",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953575893207494706/24c3af4c2c9824cda460b9368c1fcaa7.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep7.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่8",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953589896625422356/be76c30060a56bdb054b71d1708eaeaf.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep8.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่9",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953579552939065414/3393dc50b44b41d5125b499c3e3bcf55.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep9.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่10",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953583345986588672/75325d0e32d583b7915ace2c9746a07a.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep10.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่11",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953579184490442792/ba937a8d2ec26dcb9a58aeb9b8acdeac.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep11.jpg"
+    },
+{
+      "name_ep":"blueperiod ตอนที่12",
+"ep":"https://cdn.discordapp.com/attachments/953226649506283550/953594599602024458/24e3503e913fe51c9d9a39222856b0ae.mp4",
+ "subtitle":"ซับไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/blueperiod/blueperiod_ep12.jpg"
+    }
+  ]
+}
