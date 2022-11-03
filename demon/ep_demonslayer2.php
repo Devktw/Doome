054e7e246122d046d823583db3841e2fed983c19
Devktw <92218628+Devktw@users.noreply.github.com>
2022-11-03 07:22:42 +0700

    Update ep_demonslayer2.php

--- demon/ep_demonslayer2.php
+++ demon/ep_demonslayer2.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"ดาบพิฆาตอสูร เดอะมูฟวี่ ศึกรถไฟสู่นิรันดร์",
-"ep":"https://hls-nani.xyz/cdn/hls/5e17dc1ff2a7fe1ad5963310d55c848e/1/index-v1-a1.m3u8",
+"ep":"https://fvs.io/redirector?token=dmZ4Nmx5bUpTZmUyU293aXpxMG5tYzh4N3BQbXpLdk1Lb3lkalJEb3B5S09iMU5HQ2lDaHlXcTZtSElKak1vTWpwZDIxVHJrTEpvRjRwNEIrbDYyUGl0d3krN1luQzVYRDl1N0xMSHpPR1VOWVVxTXFBcWx3dDhMU0lJaUpyL0pMWmNxbG9FelZPWGFLdFFjeG1iSGo2bXpOSHQvVmZUMEx0dz06WHYyTUJSM1BKcDUwWFQwY3pXZVY3dz09rpoH",
  "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demon/demonslayer1.png"
     }
   ]
