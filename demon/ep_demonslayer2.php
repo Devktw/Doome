af6fbe81c907a052037bb30fa770274cb5f6da8d
Devktw <92218628+Devktw@users.noreply.github.com>
2022-11-04 07:17:51 +0700

    Update ep_demonslayer2.php

--- demon/ep_demonslayer2.php
+++ demon/ep_demonslayer2.php
@@ -2,7 +2,7 @@
   "listanime":[
 {
       "name_ep":"ดาบพิฆาตอสูร เดอะมูฟวี่ ศึกรถไฟสู่นิรันดร์",
-"ep":"https://a9.rukoluo.com/f/Kimetsu-no-Yaiba-Movie-Mugen-Train/Kimetsu-no-Yaiba-Movie-Mugen-Train.mp4?m=FPpVdXi3ydxpCzLksur03A&e=1667451011",
+"ep":"https://a10.rukoluo.com/f/Kimetsu-no-Yaiba-Movie-Mugen-Train/Kimetsu-no-Yaiba-Movie-Mugen-Train.mp4?m=QOT17vTudYHbNl1nqgPYhQ&e=1667531806",
  "subtitle":"พากย์ไทย", "image_ep":"https://github.com/Devktw/Doome/raw/main/demon/demonslayer1.png"
     }
   ]
