251952b4c9df415a6fbd0c23fce85030363184aa
Devktw <92218628+Devktw@users.noreply.github.com>
2022-12-07 12:40:50 +0700

    Update demon_ep.php

--- manga_demonslayer/demon_ep.php
+++ manga_demonslayer/demon_ep.php
@@ -9,7 +9,7 @@
       "seson":"ซีซั่น: 1",
       "subtitle":"แปลไทย",
       "view":"54 หน้า",
-"image_view_ep":"https://github.com/Devktw/Doome/blob/main/manga_sister/sister_view_ep1.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_demonslayer/demon_ep1.jpg"
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_demonslayer/demon_view_ep1.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_demonslayer/demon_ep1.jpg"
    },
 {
       "name_manga_ep":"ตอนที่2",
