8661a8423db427ade5f4c7a66b2587a21c585ecb
Devktw <92218628+Devktw@users.noreply.github.com>
2022-12-07 10:07:00 +0700

    Update and rename O to demon_ep.php

---
+++ manga_demonslayer/demon_ep.php
@@ -0,0 +1,18 @@
+{
+  "listmanga":[
+{
+
+      "name_manga_ep":"ตอนที่1",
+      "ep_url":"ooo",
+      "year":"ปี: 2016",
+      "formation":"แนว: ดราม่า,ตลก,ต่อสู้",
+      "valus":"26 ตอน(จบแล้ว)",
+      "seson":"ซีซั่น: 1",
+      "subtitle":"แปลไทย",
+      "view":"54 หน้า",
+"image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_view_ep1.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_sister/sister_ep1.jpg"
+
+    }
+]
+}
+
