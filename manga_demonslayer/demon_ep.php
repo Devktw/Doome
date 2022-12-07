42cf926291cc3d38133da3fc7d1e464988f619fe
Devktw <92218628+Devktw@users.noreply.github.com>
2022-12-07 11:14:34 +0700

    Update demon_ep.php

--- manga_demonslayer/demon_ep.php
+++ manga_demonslayer/demon_ep.php
@@ -3,16 +3,15 @@
 {
 
       "name_manga_ep":"ตอนที่1",
-      "ep_url":"ooo",
+      "ep_url":"",
       "year":"ปี: 2016",
-      "formation":"แนว: ดราม่า,ตลก,ต่อสู้",
+      "formation":"แนว: แอ็คชั่น",
       "valus":"26 ตอน(จบแล้ว)",
       "seson":"ซีซั่น: 1",
       "subtitle":"แปลไทย",
       "view":"54 หน้า",
 "image_view_ep":"https://github.com/Devktw/Doome/raw/main/manga_demonslayer/demon_view_ep1.php", "image_manga_ep":"https://github.com/Devktw/Doome/raw/main/manga_demonslayer/demon_ep1.jpg"
-
-    }
-]
+}
+  ]
 }
 
