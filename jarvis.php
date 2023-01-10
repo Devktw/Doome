935772fb762ec82e8b5b50528b6f40a9e87ac224
Devktw <92218628+Devktw@users.noreply.github.com>
2023-01-10 09:13:06 +0700

    Create jarvis.php

---
+++ jarvis.php
@@ -0,0 +1,42 @@
+[  
+   {  
+      "id":"1",
+      "title":"Coco"
+   },
+   {  
+      "id":"2",
+       "title":"Terminator 2: Judgment Day 3D"
+   },
+   {  
+      "id":"3",
+      "title":"Dunkirk"
+   },
+   {  
+      "id":"4",
+       "title":"The Salesman"
+   },
+   {  
+      "id":"5",
+      "title":"Lion"
+   },
+   {  
+      "id":"6",
+      "title":"Star Wars: The Last Jedi"
+   },
+   {  
+      "id":"7",
+      "title":"Thor: Ragnarok"
+   },
+   {  
+      "id":"8",
+      "title":"Blade Runner 2049"
+   },
+   {  
+      "id":"9",
+      "title":"Borg McEnroe"
+   },
+   {  
+      "id":"10",
+      "title":"Wonder"
+   }
+]
